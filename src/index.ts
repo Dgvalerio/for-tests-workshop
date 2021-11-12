7e3971c9944af2a5783a61168662e779659771a0
dgvalerio <al.davigvalerio@gmail.com>
2021-11-11 22:51:04 -0300

    feat(user): use user methods on index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const sum = (number1: number, number2: number): number => number1 + number2;
+import faker from 'faker';
 
-console.log({ sum: sum(7, 5) });
+import { UserController } from './user/UserController';
+
+(async () => {
+  const user = new UserController();
+
+  const fromCreate = await user.create({
+    birth: faker.date.past(),
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+  });
+
+  const fromShow = await user.show(fromCreate.id);
+
+  const fromList = await user.list();
+
+  const fromUpdate = await user.update(fromCreate.id, {
+    name: faker.name.findName(),
+  });
+
+  await user.delete(fromList[0].id);
+
+  console.log({ fromCreate, fromShow, fromList, fromUpdate });
+})();
